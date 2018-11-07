b2cee30afa883a0146a98a4a66f9966692f6f14f
Jennifer Browning <browningjen@yahoo.com>
2018-11-07 10:30:24 -0500

    Updates

---
+++ precache-manifest.6e928f6fd06f131d3078b12275543664.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-gh-pages/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "346eb3eec4c1b18bf79d",
+    "url": "/react-gh-pages/static/js/runtime~main.346eb3ee.js"
+  },
+  {
+    "revision": "cdd0cd3a1c0b6540725c",
+    "url": "/react-gh-pages/static/js/main.cdd0cd3a.chunk.js"
+  },
+  {
+    "revision": "1a2fe23bbee4e72efb16",
+    "url": "/react-gh-pages/static/js/1.1a2fe23b.chunk.js"
+  },
+  {
+    "revision": "cdd0cd3a1c0b6540725c",
+    "url": "/react-gh-pages/static/css/main.e9e91766.chunk.css"
+  },
+  {
+    "revision": "3bfcbc74fc53e40875f673a3676ce01e",
+    "url": "/react-gh-pages/index.html"
+  }
+];
